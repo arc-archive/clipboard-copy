2f5bb4f8105b4031bab40b1fede696b62171a498
Pawel Psztyc <jarrodek@gmail.com>
2019-03-25 12:17:27 -0700

    Update: Updating import paths to match web spec

--- clipboard-copy.js
+++ clipboard-copy.js
@@ -11,8 +11,8 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+import {PolymerElement} from '../../@polymer/polymer/polymer-element.js';
+import {html} from '../../@polymer/polymer/lib/utils/html-tag.js';
 /**
  * An element that copies a text to clipboard.
  *
